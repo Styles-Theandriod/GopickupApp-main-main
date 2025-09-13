6f702c10730e0a8766abffc2d501625c8099d70f
Styles-Theandriod <emmanuelajah188@gmail.com>
2025-09-13 19:10:44 +0100

    removed status bar

--- app/tabs/home.tsx
+++ app/tabs/home.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 
 export default function Home() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F8F9F8' }}>
+        <View style={{ flex: 1,  alignItems: 'center', backgroundColor: '#F8F9F8' }}>
+                  <StatusBar hidden={true} />
             <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Welcome to the Home Screen!</Text>
         </View>
     );
